9c462ed078289392617ae84054786fb2c8c1f117
Jens Schyma <jeschyma@gmail.com>
2016-01-03 01:11:09 +0100

    small Changes to dev because Express Module updated
    app.configure was used before
    however express 4.* does not support this anymore - so i removed it here

--- dev/Run_FinTSServer.js
+++ dev/Run_FinTSServer.js
@@ -1,6 +1,6 @@
-/*     
+/*
  * 	  Copyright 2015 Jens Schyma jeschyma@gmail.com
- *		
+ *
  *	  This File is a Part of the source of Open-Fin-TS-JS-Client.
  *
  *    This program is free software: you can redistribute it and/or  modify
@@ -16,7 +16,7 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  *	  Please contact Jens Schyma if you are interested in a commercial license.
- *	
+ *
  */
 var express = require('express');
 var cors = require('cors');
@@ -27,8 +27,8 @@ var https = require("https");
 var url = require("url");
 var fs = require('fs');
 
-var ipaddr  = process.env.IP || "127.0.0.1";//process.env.IP;
-var port      = process.env.PORT || 3000;//process.env.PORT;
+var ipaddr = process.env.IP || "127.0.0.1"; //process.env.IP;
+var port = process.env.PORT || 3000; //process.env.PORT;
 var app = express();
 var myFINTSServer = new FinTSServer();
 var myFINTSServer22 = new FinTSServer();
@@ -36,91 +36,89 @@ myFINTSServer22.proto_version = 220;
 
 app.use(
 	cors({
-		origin: true,
-		credentials: true
-	})
-);
-app.configure(function () {
-	app.get("/",function(req, res){
-		res.setHeader('Content-Type', 'text/html');
-		res.send("Test FinTS Server - at /cgi-bin/hbciservlet und BLZ = 12345678");
+		origin : true,
+		credentials : true
+	}));
+
+app.get("/", function (req, res) {
+	res.setHeader('Content-Type', 'text/html');
+	res.send("Test FinTS Server - at /cgi-bin/hbciservlet und BLZ = 12345678");
+});
+
+app.post("/cgi-bin/hbciservlet", function (req, res) {
+	textBody(req, res, function (err, body) {
+		// err probably means invalid HTTP protocol or some shiz.
+		if (err) {
+			res.statusCode = 500;
+			return res.end("NO U");
+		}
+		res.setHeader('Content-Type', 'text/plain');
+		res.send(myFINTSServer.handleIncomeMessage(body));
 	});
-	
-	app.post("/cgi-bin/hbciservlet",function(req, res){
-		textBody(req, res, function (err, body) {
-			// err probably means invalid HTTP protocol or some shiz.
-			if (err) {
-				res.statusCode = 500;
-				return res.end("NO U");
-			}
-			res.setHeader('Content-Type', 'text/plain');
-			res.send(myFINTSServer.handleIncomeMessage(body));
-		});
-		
+
+});
+app.post("/cgi-bin/hbciservlet22", function (req, res) {
+	textBody(req, res, function (err, body) {
+		// err probably means invalid HTTP protocol or some shiz.
+		if (err) {
+			res.statusCode = 500;
+			return res.end("NO U");
+		}
+		res.setHeader('Content-Type', 'text/plain');
+		res.send(myFINTSServer.handleIncomeMessage(body));
 	});
-	app.post("/cgi-bin/hbciservlet22",function(req, res){
-		textBody(req, res, function (err, body) {
-			// err probably means invalid HTTP protocol or some shiz.
-			if (err) {
-				res.statusCode = 500;
-				return res.end("NO U");
+});
+
+app.post("/cgi-bin/hbciservlet_proxy", function (req2, res2) {
+	textBody(req2, res2, function (err, body) {
+		// err probably means invalid HTTP protocol or some shiz.
+		if (err) {
+			res2.statusCode = 500;
+			return res2.end("NO U");
+		}
+		// create a connection
+		var post_data = body;
+		var clear_txt_2 = new Buffer(body, 'base64').toString('utf8');
+		console.log("Send: " + clear_txt_2);
+		fs.appendFileSync("log_proxy_msg.txt", "Send: " + clear_txt_2 + "\n\r");
+		var u = url.parse("TODO");
+		var options = {
+			hostname : u.hostname,
+			port : u.port,
+			path : u.path,
+			method : 'POST',
+			headers : {
+				'Content-Type' : 'text/plain',
+				'Content-Length' : post_data.length
 			}
-			res.setHeader('Content-Type', 'text/plain');
-			res.send(myFINTSServer.handleIncomeMessage(body));
-		});
-	});
-		
-	app.post("/cgi-bin/hbciservlet_proxy",function(req2, res2){
-		textBody(req2, res2, function (err, body) {
-			// err probably means invalid HTTP protocol or some shiz.
-			if (err) {
-				res2.statusCode = 500;
-				return res2.end("NO U");
-			}
-			// create a connection
-			var post_data = body;
-			var clear_txt_2 = new Buffer(body, 'base64').toString('utf8');
-			console.log("Send: "+clear_txt_2);
-			fs.appendFileSync("log_proxy_msg.txt","Send: "+clear_txt_2+"\n\r");
-			var u = url.parse("TODO");
-			var options = {
-			  hostname: u.hostname,
-			  port: u.port,
-			  path: u.path,
-			  method: 'POST',
-			  headers: {
-	          'Content-Type': 'text/plain',
-	          'Content-Length': post_data.length
-			  }
-			};
-			var data = "";
-			var req = https.request(options, function(res) {
-		  	res.on('data', function (chunk) {
+		};
+		var data = "";
+		var req = https.request(options, function (res) {
+				res.on('data', function (chunk) {
 					data += chunk;
-		  	});
-		  	res.on('end', function(){
+				});
+				res.on('end', function () {
 					var clear_txt = new Buffer(data, 'base64').toString('utf8');
-					console.log("Recv: "+clear_txt);
-					fs.appendFileSync("log_proxy_msg.txt","Recv: "+clear_txt+"\n\r");
+					console.log("Recv: " + clear_txt);
+					fs.appendFileSync("log_proxy_msg.txt", "Recv: " + clear_txt + "\n\r");
 					res2.setHeader('Content-Type', 'text/plain');
 					res2.send(data);
-		  	});
-		});
-		
-		req.on('error', function(){
+				});
+			});
+
+		req.on('error', function () {
 			// Hier wird dann weiter gemacht :)
 			res2.end();
-		  });
+		});
 		req.write(post_data);
 		req.end();
-			
-		});
+
 	});
 });
 
 var server = http.createServer(app);
-console.log('Listening at IP ' + ipaddr +' on port '+port);
-server.listen(port,ipaddr, function(){
-  var addr = server.address();
-  console.log("FinTS server running at:", addr.address + ":" + addr.port+"/cgi-bin/hbciservlet");
+console.log('Listening at IP ' + ipaddr + ' on port ' + port);
+server.listen(port, ipaddr, function () {
+	var addr = server.address();
+	console.log("FinTS server running at:", addr.address + ":" + addr.port + "/cgi-bin/hbciservlet");
 });
